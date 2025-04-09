35e269a841075c58f7871ef46adb9d286e1aa7f2
solution planets <foram.doshi@solutionplanets.com>
2025-04-09 08:21:46 -0500

    Style login regester page

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,33 @@
-import axios from 'axios';
-import './App.css';
+/* import axios from 'axios'; */
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+/* import './App.css'; */
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
 
 //data will be the string we send from our server
-const apiCall = () => {
+/* const apiCall = () => {
   axios.get('http://localhost:8080').then((data) => {
     //this console.log will be in our frontend console
     console.log(data)
   })
-}
+} */
 
 function App() {
+  
   return (
-    <div className="App">
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+
+    /*{<div className="App">
       <header className="App-header">
       <button onClick={apiCall}>Make API Call</button>
       </header>
-    </div>
+    </div> }*/
   );
 }
 
